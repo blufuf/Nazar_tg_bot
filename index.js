7e7fae3e37d9eb5f3f14dceb976cb059311173a2
Blufuf <Ilyafatkincool@yandex.ru>
2025-04-03 14:15:37 +0300

    мс

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+    res.writeHead(200);
+    res.end('Bot is alive');
+});
+server.listen(process.env.PORT || 3000);
+
 const TelegramBot = require('node-telegram-bot-api')
 
 
